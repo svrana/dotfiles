9fa9f83f844d4f778f3f99887aa2e009320e10e0
Shaw Vrana <shaw@vranix.com>
2015-05-08 11:14:45 -0700

    Removed some files no longer needed
    Added bitsof script

--- scripts/remove-eclipse-snap.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-rm -f ~/workspace/.metadata/.plugins/org.eclipse.core.resources/.snap
--- scripts/remove-eclipse-workspace-lock.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-#rm -f ~/workspace/.metadata/.plugins/org.eclipse.core.resources/.snap
-rm -f ~/workspace/.metadata/.lock
