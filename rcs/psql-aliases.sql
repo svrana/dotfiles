96960218aebec638c4a8bc87f293f914f6ea10e1
Shaw Vrana <shaw@vranix.com>
2015-08-24 10:18:40 -0700

    Add command to show defined aliases

---
+++ rcs/psql-aliases.sql
@@ -0,0 +1,9 @@
+\echo '\nAliases:\n'
+\echo '\t:users\t\tShow users'
+\echo '\t:conninfo\tServer connections'
+\echo '\t:showlocks\tShow table locks'
+\echo '\t:showrunning\tShow running processes'
+\echo '\t:tablesize\tTable sizes'
+\echo '\t:usergroups\tShow users and their groups'
+\echo '\t:groupprivs\tShow group priveleges'
+\echo
