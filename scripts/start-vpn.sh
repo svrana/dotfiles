2bb400547c752941f9def7dd9206e286ffcb8237
Shaw Vrana <shaw@vranix.com>
2015-09-05 19:56:00 -0700

    vpn

--- scripts/start-vpn.sh
+++ scripts/start-vpn.sh
@@ -1,1 +1,1 @@
-sudo openvpn --config $VPN_DIR/client.ovpn --up /etc/openvpn/update-resolv-conf --down /etc/openvpn/update-resolv-conf --script-security 2
+sudo openvpn --config $VPN_DIR/client.conf --up /etc/openvpn/update-resolv-conf --down /etc/openvpn/update-resolv-conf --script-security 2
