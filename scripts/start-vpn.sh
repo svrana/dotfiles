aff7bc4f2af423e291406e295ada947bb14371cf
Shaw Vrana <shaw@vranix.com>
2017-04-04 09:37:17 -0700

    More virtualenv and vpn dir tweaks

--- scripts/start-vpn.sh
+++ scripts/start-vpn.sh
@@ -1,1 +1,1 @@
-sudo openvpn --config $VPN_DIR/client.conf --up /etc/openvpn/update-resolv-conf --down /etc/openvpn/update-resolv-conf --script-security 2
+sudo openvpn --config ~/Documents/wish/vpn/client.conf --up /etc/openvpn/update-resolv-conf --down /etc/openvpn/update-resolv-conf --script-security 2
