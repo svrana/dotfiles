ad8d736fc5d57ee4fab14c83eb4e71e2888e2a00
Shaw Vrana <shaw@vranix.com>
2017-04-28 17:34:05 -0700

    Remove some old commands no longer used

--- scripts/hide-spotify.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-wget http://raw.github.com/ozcanesen/spotify-indicator/master/spotify -O /tmp/spotify
-sudo cp /tmp/spotify /etc/apparmor.d/
-sudo service apparmor reload
--- scripts/screenshot.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-filename=screenshot.png
-if [ ! -z "$1" ]; then
-    filename=$1
-fi
-
-java -jar ~/apps/android-screenshot.jar -d $filename
-ls -al $filename
