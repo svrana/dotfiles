cead4bf42588c03fef5dba2885a1fab799f6058f
Shaw Vrana <shaw@vranix.com>
2017-05-15 21:27:55 -0700

    More dev packages

--- chef/cookbooks/devbox/recipes/packages.rb
+++ chef/cookbooks/devbox/recipes/packages.rb
@@ -15,4 +15,18 @@ package %w(
   exuberant-ctags
   silversearcher-ag
   curl
+  libcurl4-openssl-dev
+  libcap-dev
+  libxml2-dev
+  libxslt-dev
+  libldap2-dev
+  libidn11-dev
+  librtmp-dev
+  libkrb5-dev
+  python-m2crypto
+  libsasl2-dev
+  libssl-dev
+  swig
+  network-manager-openvpn
+  network-manager-openvpn-gnome
 )
