dd639db3c7edfd92b478d26f1bee0b65bef93772
Shaw Vrana <shaw@vranix.com>
2017-09-08 17:48:02 -0700

    perms first

--- plugins/powerline-go.sh
+++ plugins/powerline-go.sh
@@ -1,5 +1,5 @@
 function _update_ps1() {
-    PS1="$(powerline-go -theme solarized -modules "user,venv,ssh,cwd,perms")"
+    PS1="$(powerline-go -theme solarized -modules "perms,user,venv,ssh,cwd")"
 }
 
 PROMPT_COMMAND="_update_ps1 ; $PROMPT_COMMAND"
