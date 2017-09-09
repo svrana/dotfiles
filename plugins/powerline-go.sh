e727851a5421deeaee1302e28518d3dd82260c3d
Shaw Vrana <shaw@vranix.com>
2017-09-09 10:55:21 -0700

    Set max number of dirs to show

--- plugins/powerline-go.sh
+++ plugins/powerline-go.sh
@@ -1,5 +1,5 @@
 function _update_ps1() {
-    PS1="$(powerline-go -theme solarized -modules "perms,user,venv,ssh,cwd")"
+    PS1="$(powerline-go -theme solarized -modules "perms,user,venv,ssh,cwd" -cwd-max-depth 3)"
 }
 
 PROMPT_COMMAND="_update_ps1 ; $PROMPT_COMMAND"
