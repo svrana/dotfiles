d00e71a8898a27a2c04ad5a679c0acb4e4f37270
Shaw Vrana <shaw@vranix.com>
2017-09-20 20:38:08 -0700

    Reinitialize direnv after sourcing bashrc
    mtime not changing fast enough

--- plugins/dotfiles.sh
+++ plugins/dotfiles.sh
@@ -1,4 +1,4 @@
-bind '"\e[18~":"source ~/.bashrc\n"'    # source bashrc with F7
+bind '"\e[18~":"source ~/.bashrc && direnv allow > /dev/null 2>&1\n"'    # source bashrc with F7
 
 alias cdd='cd $DOTFILES_DIR'
 alias cvi='vi $DOTFILES_DIR'
