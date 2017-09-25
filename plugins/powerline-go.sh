1200d42554dc6fee74126b1a8672452604d135fd
Shaw Vrana <shaw@vranix.com>
2017-09-25 16:21:56 -0700

    priority

--- plugins/powerline-go.sh
+++ plugins/powerline-go.sh
@@ -1,5 +1,5 @@
 function _update_ps1() {
-    local priority='root,cwd,user,perms,git-branch'
+    local priority='root,user,perms,virtualenv,git-branch,cwd'
     local modules='perms,user,venv,gitlite,ssh,cwd'
     PS1="$(powerline-go -theme solarized -modules ${modules} -priority ${priority} -cwd-max-depth 3)"
 }
