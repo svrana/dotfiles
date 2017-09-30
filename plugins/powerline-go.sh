be75271e2a79cb7d943996113f1ff0a8a4c0c660
Shaw Vrana <shaw@vranix.com>
2017-09-29 20:05:04 -0700

    Width 65%

--- plugins/powerline-go.sh
+++ plugins/powerline-go.sh
@@ -2,7 +2,7 @@ function _update_ps1() {
     local priority='root,user,perms,virtualenv,git-branch,cwd'
     local modules='perms,user,venv,gitlite,ssh,cwd'
     PS1="$(powerline-go -theme solarized -modules ${modules} \
-        -priority ${priority} -cwd-max-depth 3 -max-width 60)"
+        -priority ${priority} -cwd-max-depth 3 -max-width 65)"
 }
 
 PROMPT_COMMAND="_update_ps1 ; $PROMPT_COMMAND"
