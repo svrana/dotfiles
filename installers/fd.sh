cc507c7721c53729bf9f97ad5b6ebc34d49e0e2b
Shaw Vrana <shaw@vranix.com>
2017-11-24 14:04:29 -0800

    Installer for fd

---
+++ installers/fd.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ ! -f "$BIN_DIR/" ]; then
+    VERSION="v6.0.0"
+    FILENAME=fd-${VERSION}-x86_64-unknown-linux-musl.tar.gz
+    DIRNAME=${FILENAME%.tar.gz}
+
+    wget -P "$DOWNLOADS" "https://github.com/sharkdp/fd/releases/download/$VERSION/$FILENAME"
+    tar xzvf "$DOWNLOADS/$FILENAME" -C "$DOWNLOADS"
+    cp "$DOWNLOADS/$DIRNAME/fd" "$BIN_DIR"
+    rm -r "${DOWNLOADS:?}/$DIRNAME"
+    rm "$DOWNLOADS/$FILENAME"
+
+    unset VERSION
+    unset FILENAME
+    unset DIRNAME
+fi
