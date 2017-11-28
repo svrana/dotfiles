2bf53f34f00540fab0b901fcd10b91569c7323ce
Shaw Vrana <shaw@vranix.com>
2017-11-28 11:36:48 -0800

    Fix install function name

--- plugins/virtualgo.sh
+++ plugins/virtualgo.sh
@@ -2,8 +2,8 @@
 
 command -v vg >/dev/null 2>&1 && eval "$(vg eval --shell bash)"
 
-__dotfiles_install_virtualgo() {
-    if ! which vg ; then
+__dotfiles_virtualgo_install() {
+    if ! which vg > /dev/null ; then
         go get -u github.com/GetStream/vg
     fi
 }
