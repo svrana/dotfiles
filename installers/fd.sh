fbdcec8d2db9734b0ca5af6e1bc554802b383670
Shaw Vrana <shaw@vranix.com>
2017-11-28 11:35:31 -0800

    Correct existence check

--- installers/fd.sh
+++ installers/fd.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ ! -f "$BIN_DIR/" ]; then
+if [ ! -f "$BIN_DIR/fd" ]; then
     VERSION="v6.0.0"
     FILENAME=fd-${VERSION}-x86_64-unknown-linux-musl.tar.gz
     DIRNAME=${FILENAME%.tar.gz}
