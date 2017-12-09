7d606ba6f48457ac12acc0a9243052cdbca5c064
Shaw Vrana <shaw@vranix.com>
2017-12-08 17:05:16 -0800

    moah

--- installers/tmux.sh
+++ installers/tmux.sh
@@ -7,5 +7,6 @@ if [ ! -d ~/.tmux/plugins/tpm ]; then
     git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 fi
 
+echo
 ~/.tmux/plugins/tpm/bin/install_plugins
 ~/.tmux/plugins/tpm/bin/clean_plugins
