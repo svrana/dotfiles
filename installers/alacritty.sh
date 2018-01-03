51d179985f3876d49aa600957ff06ca6d256b8b1
Shaw Vrana <shaw@vranix.com>
2018-01-03 12:27:15 -0800

    alacritty

--- installers/alacritty.sh
+++ installers/alacritty.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-mkdir -p ~/.config/alacritty
-
-ln -sf "${RCS}/alacritty.yml" ~/.config/alacritty/alacritty.yml
-ln -sf "${DOTFILES}/misc/alacritty.desktop" ~/.local/share/applications
-
 if [ ! -d "$APPS/alacritty" ]; then
     git clone https://github.com/jwilm/alacritty "$APPS/alacritty"
     pushd "$APPS/alacritty"
@@ -12,5 +7,4 @@ if [ ! -d "$APPS/alacritty" ]; then
     rustup update stable
     cargo build --release
     popd
-    ln -sf "$APPS/alacritty/target/release/alacritty" "$BIN_DIR/alacritty"
 fi
