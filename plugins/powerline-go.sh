a5ecbc27eb7d7af84dd3835175ff308bca99cde9
Shaw Vrana <shaw@vranix.com>
2018-01-16 10:42:51 -0800

    Add virtualgo segment

--- plugins/powerline-go.sh
+++ plugins/powerline-go.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 function _update_ps1() {
-    local priority='root,user,perms,virtualenv,git-branch,cwd'
-    local modules='perms,user,venv,gitlite,ssh,cwd'
+    local priority='root,user,perms,virtualenv,vgo,git-branch,cwd'
+    local modules='perms,user,venv,vgo,gitlite,ssh,cwd'
     PS1="$(powerline-go -theme solarized -modules ${modules} \
         -priority ${priority} -cwd-max-depth 3 -max-width 65)"
 }
