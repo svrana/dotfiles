0561deb3fc385bb889838fbf7c1026ba667d2ba1
Shaw Vrana <shaw@vranix.com>
2018-03-08 11:12:37 -0800

    Add path alias for $GOPATH/src/github.com

--- plugins/powerline-go.sh
+++ plugins/powerline-go.sh
@@ -4,7 +4,9 @@ function _update_ps1() {
     local priority='root,user,perms,virtualenv,vgo,git-branch,cwd'
     local modules='perms,user,venv,vgo,gitlite,ssh,cwd'
     PS1="$(powerline-go -theme solarized -modules ${modules} \
-        -priority ${priority} -cwd-max-depth 3 -max-width 65)"
+        -priority ${priority} -cwd-max-depth 3 -max-width 65 \
+        -path-aliases \~/go/src/github.com=@go-gh
+        )"
 }
 
 PROMPT_COMMAND="_update_ps1 ; $PROMPT_COMMAND"
