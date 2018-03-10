4dc805c09e59cbb61709ec28f95a0a97f8e398ef
Shaw Vrana <shaw@vranix.com>
2018-03-09 20:10:14 -0800

    Check for command name not binary existence

--- installers/fd.sh
+++ installers/fd.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-if [ ! -f "$BIN_DIR/fd" ]; then
+if ! command -v fd > /dev/null ; then
     VERSION="6.3.0"
     FILENAME=fd-musl_${VERSION}_amd64.deb
 
     wget -P "$APPS" "https://github.com/sharkdp/fd/releases/download/v$VERSION/$FILENAME"
     sudo dpkg -i "$APPS/$FILENAME"
+
+    rm "$APPS/$FILENAME"
 
     unset VERSION
     unset FILENAME
