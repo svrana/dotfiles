130fb771d45b300e0f83fe9dfd68d8731fdda4a0
Shaw Vrana <shaw@vranix.com>
2018-03-13 14:52:46 -0700

    Add script to set it all up

--- load.sh
+++ load.sh
@@ -2,9 +2,13 @@
 
 function _load_deps() {
     CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-    source "$CURRENT_DIR/directories.sh"
     source "$CURRENT_DIR/functions.sh"
+    source "$CURRENT_DIR/directories.sh"
+}
+
+function _scaffold() {
+    source "$PROJECTS/bash-home-scaffold/load.sh"
 }
 
 _load_deps
-source "$PROJECTS/bash-home-scaffold/load.sh"
+_scaffold
