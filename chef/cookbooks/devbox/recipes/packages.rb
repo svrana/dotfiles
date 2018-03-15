33e0b3b9a6970931ab002c63edfd7c220bec9ce2
Shaw Vrana <shaw@vranix.com>
2018-03-15 15:12:45 -0700

    Add salt repo
    No packages yet

--- chef/cookbooks/devbox/recipes/packages.rb
+++ chef/cookbooks/devbox/recipes/packages.rb
@@ -33,8 +33,15 @@ end
 apt_repository 'yarn' do
   uri 'https://dl.yarnpkg.com/debian'
   key 'https://dl.yarnpkg.com/debian/pubkey.gpg'
-  components ['stable', 'main']
+  components %w(stable main)
   distribution ''
+end
+
+apt_repository 'salt' do
+  uri 'http://repo.saltstack.com/apt/ubuntu/16.04/amd64/latest'
+  key 'https://repo.saltstack.com/apt/ubuntu/16.04/amd64/latest/SALTSTACK-GPG-KEY.pub'
+  distribution node['lsb']['codename']
+  components ['main']
 end
 
 package %w(
