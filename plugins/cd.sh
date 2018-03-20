8c8e6233eaf6d68fbaa0220a81c1ac104a5c99af
Shaw Vrana <shaw@vranix.com>
2018-03-20 13:42:28 -0700

    Correct depth of gopath cdp

--- plugins/cd.sh
+++ plugins/cd.sh
@@ -8,6 +8,6 @@ alias ....='cd ../../../'
 alias .....='cd ../../../../'
 
 
-export CDP_DIR_SPEC="$HOME/Projects:2;$GOPATH/src/github.com:3"
+export CDP_DIR_SPEC="$HOME/Projects:3;$GOPATH/src/github.com:2"
 export CDP_DEFAULT_VAR="GRUF_PROJECT"
 source "$PROJECTS/cdp/cdp.sh"
