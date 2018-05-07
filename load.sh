31ff39bf3894d05e60b1684db5d4a9b55895d2f5
Shaw Vrana <shaw@vranix.com>
2018-05-06 17:50:44 -0700

    Hide CURRENT_DIR

--- load.sh
+++ load.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 function _load_deps() {
-    CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+    local CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
     source "$CURRENT_DIR/functions.sh"
     source "$CURRENT_DIR/directories.sh"
 }
