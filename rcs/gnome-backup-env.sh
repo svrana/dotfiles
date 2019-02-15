565242cb634f054afb9197c3bfad8fd392054853
Shaw Vrana <shaw@vranix.com>
2019-02-15 14:32:12 -0800

    Remove gnome config backup utility

--- rcs/gnome-backup-env.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Shared configuration for gnome-backup-config.sh and gnome-restore-config.sh
-
-export GNOME3_EXT_PATH="$HOME/.local/share/gnome-shell/extensions"
-export GNOME3_DUMP_FILE="$DOTFILES/misc/gnome3-config-dump.$HOSTNAME.txt"
-export EXT_DUMP_FILE="$DOTFILES/misc/gnome_shell_extensions_id.$HOSTNAME.txt"
-export GNOME3_EXT_QUERY_URL="https://extensions.gnome.org/extension-query/?page=1&shell_version=all&search"
