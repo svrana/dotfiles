8b70fb7ee865b6f45386be9f2946346e6dd8659d
Shaw Vrana <shaw@vranix.com>
2019-02-15 14:31:00 -0800

    misc

--- plugins/k8s.sh
+++ plugins/k8s.sh
@@ -1,6 +1,7 @@
 #source <(kubectl completion bash) too slow...
 
 alias kube-token='grep id-token ~/.kube/config | cut -d':' -f2 | cut -b 2-'
+alias k='kubectl'
 
 function kube-context() {
     echo "Cluster: $(kubectl config current-context)"
