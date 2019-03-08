50dfb91b2d821d9d13fbaaa76f8a210abb05d598
Shaw Vrana <shaw@vranix.com>
2019-03-08 14:48:51 -0800

    Less chef

--- install.sh
+++ install.sh
@@ -15,6 +15,100 @@ for dep in "${_install_deps[@]}" ; do
     . "$CURRENT_DIR/$dep"
 done
 
+export PACKAGE_LIST=(
+    compton
+    autocutsel
+    build-essential
+    fonts-powerline
+    pass
+    python-pip
+    python-dev
+    python3-dev
+    python3-pip
+    git
+    tox
+    tmux
+    xclip
+    openvpn
+    openjdk-8-jdk
+    exuberant-ctags
+    silversearcher-ag
+    curl
+    libcurl4-openssl-dev
+    libcap-dev
+    libxml2-dev
+    libldap2-dev
+    libidn11-dev
+    librtmp-dev
+    libkrb5-dev
+    python-m2crypto
+    libsasl2-dev
+    libssl-dev
+    swig
+    network-manager-openvpn
+    network-manager-openvpn-gnome
+    shellcheck
+    ruby
+    ruby-dev
+    libpq-dev
+    rvm
+    spotify-client
+    zeal
+    insync
+    firefox
+    virtualenv
+    cmake
+    flex
+    libelf-dev
+    libfreetype6-dev
+    libfontconfig1-dev
+    pandoc
+    entr
+    weechat
+    yarn
+    i3
+    feh
+    xautolock
+    rofi
+    cmake
+    cmake-data
+    libcairo2-dev
+    libxcb1-dev
+    libxcb-ewmh-dev
+    libxcb-icccm4-dev
+    libxcb-image0-dev
+    libxcb-randr0-dev
+    libxcb-util0-dev
+    libxcb-xkb-dev
+    pkg-config
+    python-xcbgen
+    xcb-proto
+    libxcb-xrm-dev
+    i3-wm
+    libasound2-dev
+    libmpdclient-dev
+    libiw-dev
+    libcurl4-openssl-dev
+    libpulse-dev
+    libxcb-composite0-dev
+    notify-osd
+    mpv
+)
+
+export GLOBAL_NODE_PACKAGES=(
+    neovim
+    diff-so-fancy
+    eslint
+    tern
+    n
+)
+
+export GLOBAL_GEMS=(
+    neovim
+)
+
+
+
 # A list of installers in $DOTFILES/installers that will be sourced during
 # install. These are typically actions that need only be done once and for
 # which there might not be a corresponding plugin.
@@ -102,5 +196,39 @@ export FILE_LINKS=(
     "${DOTFILES}/scripts/qute-pass              ~/.local/share/qutebrowser/userscripts/qute-pass"
     "${RCS}/mimeapps.list                       ~/.config/mimeapps.list"
 )
+
+
+
+#apt_repository 'neovim' do
+#  uri 'ppa:neovim-ppa/stable'
+#end
+#
+#apt_repository 'rvm' do
+#  uri 'ppa:rael-gc/rvm'
+#  distribution node['lsb']['codename']
+#end
+#
+#apt_repository 'spotify' do
+#  uri 'http://repository.spotify.com'
+#  components ['stable', 'non-free']
+#  distribution ''
+#end
+#
+#apt_repository 'insync' do
+#  uri 'http://apt.insynchq.com/ubuntu'
+#  components ['non-free', 'contrib']
+#  distribution node['lsb']['codename']
+#end
+#
+#apt_repository 'yarn' do
+#  uri 'https://dl.yarnpkg.com/debian'
+#  # curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+#  # key 'https://dl.yarnpkg.com/debian/pubkey.gpg'
+#  components %w(stable main)
+#  distribution ''
+#end
+#
+
+#add_to_source_list neovim "http://ppa.launchpad.net./neovim-ppa/stable/ubuntu bionic main"
 
 source "$PROJECTS/bash-home-scaffold/install.sh" "$*"
