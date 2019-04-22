0bdbaa98cc8dced55c4731dd793b4548fd783c8d
Shaw Vrana <shaw@vranix.com>
2019-04-22 11:19:18 -0700

    Misc.

--- installers/spacemacs.sh
+++ installers/spacemacs.sh
@@ -1,6 +1,6 @@
 install() {
     if [ ! -d ~/.emacs.d ]; then
-        git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
+        git clone -b develop https://github.com/syl20bnr/spacemacs ~/.emacs.d
     fi
 }
 install
