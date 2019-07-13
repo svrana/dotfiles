d545e8a24e409d6ddea49378d808b6acfb0c32a2
Shaw Vrana <shaw@vranix.com>
2019-07-13 11:05:21 -0700

    Location changed upstream

--- installers/alacritty.sh
+++ installers/alacritty.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 install_alacritty() {
-    local VERSION="0.3.0"
-    local FILENAME="Alacritty-${VERSION}_amd64.deb"
-    local URL="https://github.com/jwilm/alacritty/releases/download/v${VERSION}/${FILENAME}"
-
+    local VERSION="0.3.3"
+    local URL="https://github.com/jwilm/alacritty/releases/download/v${VERSION}/Alacritty-v${VERSION}-ubuntu_18_04_amd64.deb"
     ext_package_install alacritty $VERSION $URL
 }
 
