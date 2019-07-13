55fb56dc585866e02b28257d077ea79dc7316ae0
Shaw Vrana <shaw@vranix.com>
2019-07-13 13:32:50 -0700

    https

--- installers/i3lock-color.sh
+++ installers/i3lock-color.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ ! -d $APPS/i3lock-color ]; then
-    git clone git@github.com:PandorasFox/i3lock-color.git $APPS/i3lock-color
+    git clone https://github.com/PandorasFox/i3lock-color.git $APPS/i3lock-color
     pushd $APPS/i3lock-color
     autoreconf -i && ./configure && cd x86_64-pc-linux-gnu && make -j8 && sudo make install
 fi
