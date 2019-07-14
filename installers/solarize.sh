280cf09a5187068ffef08ca65c2a2cd4ca3cb969
Shaw Vrana <shaw@vranix.com>
2019-07-14 09:56:38 -0700

    use forked solarize to just set the theme

--- installers/solarize.sh
+++ installers/solarize.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 if [ ! -d "$APPS/solarize" ]; then
-    git clone https://github.com/gmodarelli/solarize.git "$APPS/solarize"
+    git clone https://github.com/svrana/solarize.git "$APPS/solarize"
     "$APPS/solarize/solarize.sh" dark
 fi
