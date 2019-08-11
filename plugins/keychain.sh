e21ce7f467ea74b10d92ff604bd32c8771f2adc0
Shaw Vrana <shaw@vranix.com>
2019-08-11 11:09:23 -0700

    Remove --quiet and --quick for keychain testing

--- plugins/keychain.sh
+++ plugins/keychain.sh
@@ -1,1 +1,4 @@
 eval `keychain --quiet --quick --eval --absolute --dir $HOME/.config/keychain ~/.ssh/id_rsa ~/.ssh/bommie/id_rsa ~/.ssh/vranix/id_rsa`
+
+# remove quiet and quick to test. check perms of keys..
+#eval `keychain --eval --absolute --dir $HOME/.config/keychain ~/.ssh/id_rsa ~/.ssh/bommie/id_rsa ~/.ssh/vranix/id_rsa`
