fd57d6ce8acde447e6f5dec9ac0dfb7c1dc7c2e5
Shaw Vrana <shaw@vranix.com>
2019-08-26 12:45:56 -0700

    To test locally with dev/docker-compose

---
+++ test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# comment out https override since I do not store the keys in this repo
+sed -i '/git@github.com/, /github.com\// s/^/#/' configs/git/gitconfig
+
+dev dots sh ./install.sh
+
+# remove hack above
+sed -i '/git@github.com/, /github.com\// s/^#//' configs/git/gitconfig
