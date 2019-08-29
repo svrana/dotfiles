e31d94765b3ca8a1f4a2ac2fdda9238673e94adb
Shaw Vrana <shaw@vranix.com>
2019-08-29 14:45:59 -0700

    dotfiles directory name to match reality

--- test.sh
+++ test.sh
@@ -3,6 +3,8 @@
 # comment out https override since I do not store the keys in this repo
 sed -i '/git@github.com/, /github.com\// s/^/#/' configs/git/gitconfig
 
+dev dots sh cd ~/Projects/bash-home-scaffold && git pull
+
 dev dots sh ./install.sh
 
 # remove hack above
