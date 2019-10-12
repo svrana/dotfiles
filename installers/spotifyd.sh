aa73b73b7910a80eb4f36188abe70c0745703499
Shaw Vrana <shaw@vranix.com>
2019-10-12 13:29:17 -0700

    Skip spotify builds on ci

--- installers/spotifyd.sh
+++ installers/spotifyd.sh
@@ -1,17 +1,25 @@
-if [ ! -d "$APPS/spotifyd" ]; then
-    git clone https://github.com/Spotifyd/spotifyd.git "$APPS/spotifyd"
-    pushd "$APPS/spotifyd"
-    if cargo build --release ; then
-        cp target/release/spotifyd "$HOME/.local/bin"
+#!/usr/bin/env bash
+
+main() {
+    if [ ! -d "$APPS/spotifyd" ]; then
+        git clone https://github.com/Spotifyd/spotifyd.git "$APPS/spotifyd"
+        pushd "$APPS/spotifyd"
+        if cargo build --release ; then
+            cp target/release/spotifyd "$HOME/.local/bin"
+        fi
+        popd
     fi
-    popd
+
+    if [ ! -d "$APPS/spotify-tui" ]; then
+        git clone https://github.com/Rigellute/spotify-tui.git "$APPS/spotify-tui"
+        pushd "$APPS/spotify-tui"
+        cargo install spotify-tui
+        popd
+    fi
+}
+
+if [ -d "$TRAVIS" ]; then
+    echo "Skipping open source spotify builds on Travis"
+else
+    main
 fi
-
-if [ ! -d "$APPS/spotify-tui" ]; then
-    git clone https://github.com/Rigellute/spotify-tui.git "$APPS/spotify-tui"
-    pushd "$APPS/spotify-tui"
-    cargo install spotify-tui
-    popd
-fi
-
-
