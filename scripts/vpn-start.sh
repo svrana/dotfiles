06fa92e91b08d42f58099b2642767f169636055e
Shaw Vrana <shaw@vranix.com>
2019-10-22 11:15:18 -0700

    mod for sai

--- scripts/vpn-start.sh
+++ scripts/vpn-start.sh
@@ -1,5 +1,5 @@
-cd "$DOCUMENTS/wish/vpn" && \
-    sudo openvpn --config "$DOCUMENTS/wish/vpn/client.conf" \
+cd "$DOCUMENTS/synthesis/vpn" && \
+    sudo openvpn --config "$DOCUMENTS/synthesis/vpn/client.conf" \
     --setenv PATH '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin' \
     --script-security 2 \
     --up /etc/openvpn/scripts/update-systemd-resolved \
