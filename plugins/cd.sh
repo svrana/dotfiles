3b909a897813d1501ef9ac3706916256725ac991
Shaw Vrana <shaw@vranix.com>
2019-11-04 08:51:31 -0800

    remove go path

--- plugins/cd.sh
+++ plugins/cd.sh
@@ -9,7 +9,7 @@ alias .....='cd ../../../../'
 
 
 #export CDP_DIR_SPEC="$HOME/Projects:3;$HOME/go/src/github.com:2"
-export CDP_DIR_SPEC="$GOPATH/src/github.com:2;$HOME/Projects:3"
+export CDP_DIR_SPEC="$HOME/Projects:2"
 export CDP_DEFAULT_VAR="GRUF_PROJECT"
 export CDP_PROJECT_VAR="PROJECTS"
 source "$PROJECTS/cdp/cdp.sh"
