0e5e88a5be367a433544903d3bb35fa65be1bc98
Shaw Vrana <shaw@vranix.com>
2019-11-29 20:39:26 -0800

    Use the default powerline-go theme
    slightly modified on my fork

--- plugins/powerline-go.sh
+++ plugins/powerline-go.sh
@@ -3,7 +3,7 @@
 function _update_ps1() {
     local priority='root,user,perms,virtualenv,vgo,git-branch,cwd'
     local modules='perms,user,venv,vgo,gitlite,ssh,cwd'
-    PS1="$(powerline-go -theme solarized -modules ${modules} \
+    PS1="$(powerline-go -theme default -modules ${modules} \
         -priority ${priority} -cwd-max-depth 3 -max-width 65 \
         -path-aliases \~/go/src/github.com=@go-gh
         )"
