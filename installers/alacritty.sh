0f3346b541310835ba7867fb3d59c57c8ac7b72c
Shaw Vrana <shaw@vranix.com>
2020-01-19 12:02:35 -0800

    alacritty .4.1

--- installers/alacritty.sh
+++ installers/alacritty.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 install_alacritty() {
-    local VERSION="0.3.3"
+    local VERSION="0.4.1"
     local URL="https://github.com/jwilm/alacritty/releases/download/v${VERSION}/Alacritty-v${VERSION}-ubuntu_18_04_amd64.deb"
     ext_package_install alacritty $VERSION $URL
 }
