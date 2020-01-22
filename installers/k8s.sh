54428053512ba63b0c2a7becfbcf0a1c4e32adc1
Shaw Vrana <shaw@vranix.com>
2020-01-22 14:01:59 -0800

    Comment out kops

--- installers/k8s.sh
+++ installers/k8s.sh
@@ -16,11 +16,11 @@ function __dotfiles_install_k8s_tools() {
         chmod +x "$BIN_DIR/kubectl"
     fi
 
-    if [ ! -f "$BIN_DIR/kops" ]; then
-        local version
-        version=$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)
-        wget -O kops "https://github.com/kubernetes/kops/releases/download/$version/kops-linux-amd64" -O "$BIN_DIR/kops" && chmod +x "$BIN_DIR/kops"
-    fi
+    # if [ ! -f "$BIN_DIR/kops" ]; then
+    #     local version
+    #     version=$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)
+    #     wget -O kops "https://github.com/kubernetes/kops/releases/download/$version/kops-linux-amd64" -O "$BIN_DIR/kops" && chmod +x "$BIN_DIR/kops"
+    # fi
 }
 
 __dotfiles_install_k8s_tools
