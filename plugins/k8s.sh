1cba89babbba0d8a80faf00faab571f9af3a186d
Shaw Vrana <shaw@vranix.com>
2020-02-07 15:03:33 -0800

    bash completion for k/ubectl shortcut

--- plugins/k8s.sh
+++ plugins/k8s.sh
@@ -2,8 +2,9 @@
 
 alias kube-token='grep id-token ~/.kube/config | cut -d':' -f2 | cut -b 2-'
 alias k='kubectl'
+complete -F __start_kubectl k
 
-function kube-context() {
+kc() {
     echo "Cluster: $(kubectl config current-context)"
     echo "Namespace: $(kubectl config view | grep namespace: | cut -d':' -f2 | cut -b 2-)"
 }
