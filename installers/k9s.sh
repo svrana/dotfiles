1ef1f4fbb6cecb103e13ea207a24a5e26255ce27
Shaw Vrana <shaw@vranix.com>
2020-02-21 13:44:46 -0800

    Fix k9s skin isntall

--- installers/k9s.sh
+++ installers/k9s.sh
@@ -5,7 +5,7 @@ if [ ! -f "$BIN_DIR/k9s" ]; then
     pushd "$APPS/k9s" > /dev/null
     make build
     ln -sf "$APPS/k9s/execs/k9s" "$BIN_DIR/k9s"
-    ln -sf "$HOME/.k9s/skin.yml" "$RCS/k9s-skin.yml"
+    ln -sf "$RCS/k9s-skin.yml" "$HOME/.k9s/skin.yml"
     popd > /dev/null
 fi
 
