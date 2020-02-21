477ef4e23a068d57e592f4cd3c31e8384f4f732f
Shaw Vrana <shaw@vranix.com>
2020-02-21 10:26:50 -0800

    go 1.13.8

--- installers/go.sh
+++ installers/go.sh
@@ -6,7 +6,7 @@ GOPATH=${GOPATH:-$HOME/.go}
 function __install_golang() {
     [ -d "$GOROOT" ] && return 0
 
-    local version=1.13.5
+    local version=1.13.8
     local url=https://dl.google.com/go
     local filename=go${version}.linux-amd64.tar.gz
 
@@ -18,12 +18,12 @@ function __install_golang() {
 function __dotfiles_go_install() {
     __install_golang
 
-    local dep_version=0.5.3
-    local dep_url="https://github.com/golang/dep/releases/download/v$dep_version/dep-linux-amd64"
-    if [ ! -f ~/.local/bin/dep ]; then
-        wget ${dep_url} -O ~/.local/bin/dep
-        chmod +x ~/.local/bin/dep
-    fi
+    # local dep_version=0.5.3
+    # local dep_url="https://github.com/golang/dep/releases/download/v$dep_version/dep-linux-amd64"
+    # if [ ! -f ~/.local/bin/dep ]; then
+    #     wget ${dep_url} -O ~/.local/bin/dep
+    #     chmod +x ~/.local/bin/dep
+    # fi
 
     if [ ! -f "$GOPATH/bin/gopls" ]; then
         "$GOROOT/bin/go" get -u golang.org/x/tools/gopls
