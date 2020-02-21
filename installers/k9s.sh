5b63714143ebafaf313954d1eaeee35fbff0d99a
Shaw Vrana <shaw@vranix.com>
2020-02-20 19:13:01 -0800

    Link k9s skin

--- installers/k9s.sh
+++ installers/k9s.sh
@@ -3,7 +3,9 @@
 if [ ! -f "$BIN_DIR/k9s" ]; then
     git clone https://github.com/derailed/k9s "$APPS/k9s"
     pushd "$APPS/k9s" > /dev/null
-    make build && ln -sf "$APPS/k9s/execs/k9s" "$BIN_DIR/k9s"
+    make build
+    ln -sf "$APPS/k9s/execs/k9s" "$BIN_DIR/k9s"
+    ln -sf "$HOME/.k9s/skin.yml" "$RCS/k9s-skin.yml"
     popd > /dev/null
 fi
 
