a9b43b45303c70de62ee3c3e242d79138a10deb6
Shaw Vrana <shaw@vranix.com>
2020-02-24 10:26:42 -0800

    autoyes during install

--- installers/qutebrowser.sh
+++ installers/qutebrowser.sh
@@ -3,15 +3,13 @@
 install() {
     local dest="$APPS/qutebrowser"
     if [ ! -d "$dest" ]; then
-        sudo apt-get install asciidoc --no-install-recommends
+        sudo apt-get install asciidoc -y --no-install-recommends
         git clone https://github.com/qutebrowser/qutebrowser.git "$APPS/qutebrowser"
-        if [ -z "$TRAVIS" ]; then
-            pushd "$dest"
-            python3 scripts/mkvenv.py
-            #./scripts/asciidoc2html.py
-            scripts/dev/update_3rdparty.py
-            popd
-        fi
+        pushd "$dest"
+        python3 scripts/mkvenv.py
+        #./scripts/asciidoc2html.py
+        scripts/dev/update_3rdparty.py
+        popd
     fi
     ln -sf "$DOTFILES/misc/qutebrowser.desktop" "$HOME/.local/share/applications/qutebrowser.desktop"
 }
