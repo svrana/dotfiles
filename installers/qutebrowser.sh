f475b3283e058403211c499e509b4833cc50eea2
Shaw Vrana <shaw@vranix.com>
2020-02-24 09:10:37 -0800

    Skip qutebrowser CI compile

--- installers/qutebrowser.sh
+++ installers/qutebrowser.sh
@@ -5,11 +5,13 @@ install() {
     if [ ! -d "$dest" ]; then
         sudo apt-get install asciidoc --no-install-recommends
         git clone https://github.com/qutebrowser/qutebrowser.git "$APPS/qutebrowser"
-        pushd "$dest"
-        python3 scripts/mkvenv.py
-        #./scripts/asciidoc2html.py
-        scripts/dev/update_3rdparty.py
-        popd
+        if [ -z "$TRAVIS" ]; then
+            pushd "$dest"
+            python3 scripts/mkvenv.py
+            #./scripts/asciidoc2html.py
+            scripts/dev/update_3rdparty.py
+            popd
+        fi
     fi
     ln -sf "$DOTFILES/misc/qutebrowser.desktop" "$HOME/.local/share/applications/qutebrowser.desktop"
 }
