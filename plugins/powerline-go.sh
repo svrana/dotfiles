5dbd46631933e023bd412997203749830a930c2c
Shaw Vrana <shaw@vranix.com>
2020-02-25 14:27:01 -0800

    max path length and remove path mappings for common repos

--- plugins/powerline-go.sh
+++ plugins/powerline-go.sh
@@ -4,8 +4,8 @@ function _update_ps1() {
     local priority='root,perms,venv,git-branch,exit,cwd'
     local modules='perms,venv,gitlite,ssh,cwd,exit'
     PS1="$(powerline-go -theme default -modules ${modules} \
-        -priority ${priority} -cwd-max-depth 3 -max-width 65 \
-        -path-aliases \~/go/src/github.com=@go-gh,\~/Projects/aws-infra=aws-infra,\~/Projects/apis=apis
+        -priority ${priority} -cwd-max-depth 1 -max-width 65 \
+        -path-aliases \~/go/src/github.com=@go-gh
         )"
 }
 
