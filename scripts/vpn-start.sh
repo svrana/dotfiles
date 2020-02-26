322c33ca85493d0b05a358eda7d3d7ca6e66b2ac
Shaw Vrana <shaw@vranix.com>
2020-02-26 15:12:35 -0800

    modify paths and take an environment as parameter

--- scripts/vpn-start.sh
+++ scripts/vpn-start.sh
@@ -1,5 +1,7 @@
-cd "$DOCUMENTS/synthesis/vpn" && \
-    sudo openvpn --config "$DOCUMENTS/synthesis/vpn/client.conf" \
+env=${1:-stage}
+
+cd "$DOCUMENTS/synthesis/vpns/${env}" && \
+    sudo openvpn --config "$DOCUMENTS/synthesis/vpns/${env}/client.conf" \
     --setenv PATH '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin' \
     --script-security 2 \
     --up /etc/openvpn/scripts/update-systemd-resolved \
