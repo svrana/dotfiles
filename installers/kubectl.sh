1a1d31b1f03d8c5768a6698c22ac18f477410d98
Shaw Vrana <shaw@vranix.com>
2020-03-16 19:40:44 -0700

    make sure kubectl bash completion always setup

--- installers/kubectl.sh
+++ installers/kubectl.sh
@@ -6,4 +6,5 @@ if [ ! -f "$BIN_DIR/kubectl" ]; then
         https://storage.googleapis.com/kubernetes-release/release/$version/bin/linux/amd64/kubectl \
         -O "$BIN_DIR/kubectl" && chmod +x "$BIN_DIR/kubectl"
     chmod +x "$BIN_DIR/kubectl"
+    kubectl completion bash | sudo tee /etc/bash_completion.d/kubectl > /dev/null
 fi
