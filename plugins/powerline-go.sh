b2b1096e1a812b5d7270c25124cda1b27ddc3b7f
Shaw Vrana <shaw@vranix.com>
2020-03-18 17:03:55 -0700

    prompt tweak

--- plugins/powerline-go.sh
+++ plugins/powerline-go.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+# For single left prompt
 function _update_ps1() {
     local priority='root,perms,venv,git-branch,exit,cwd'
     local modules='perms,venv,gitlite,ssh,cwd,exit'
-    PS1="$(powerline-go -theme default -modules ${modules} \
+    PS1="$(powerline-go -cwd-mode dironly -theme default -modules ${modules} \
         -priority ${priority} -cwd-max-depth 1 -max-width 65 \
-        -path-aliases \~/go/src/github.com=@go-gh
-        )"
+        -path-aliases \~/go/src/github.com=@go-gh)"
 }
+
 
 PROMPT_COMMAND="_update_ps1 ; $PROMPT_COMMAND"
