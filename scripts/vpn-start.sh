711692ac0d6a50f822cc192568794ed5a0e0319c
Shaw Vrana <shaw@vranix.com>
2020-04-24 21:31:51 -0700

    misc

--- scripts/vpn-start.sh
+++ scripts/vpn-start.sh
@@ -1,7 +1,5 @@
-env=${1:-stage}
-
-cd "$DOCUMENTS/synthesis/vpns/${env}" && \
-    sudo openvpn --config "$DOCUMENTS/synthesis/vpns/${env}/client.conf" \
+cd "$DOCUMENTS/synthesis/vpns" && \
+    sudo openvpn --config "$DOCUMENTS/synthesis/vpns/client.conf" \
     --setenv PATH '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin' \
     --script-security 2 \
     --up /etc/openvpn/scripts/update-systemd-resolved \
